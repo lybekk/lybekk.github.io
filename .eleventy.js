b5840caa0357d8cca85eb54be949000e9b948487
lybekk <christoffer@lybekk.tech>
2021-01-29 23:08:55 +0100

    config update

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,16 @@
 const fs = require("fs");
+const { DateTime } = require("luxon");
 const CleanCSS = require("clean-css");
+var kebabCase = require('lodash/fp/kebabCase');
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 
 module.exports = function(eleventyConfig) {
 
   const passthroughList = [
     "google93445b09dad855a5.html",
     "lybekk.png",
+    "source/js/*",
     "source/img/*.png",
     "source/fluenticons",
     "source/CNAME",
@@ -16,8 +21,63 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy(item);
   }
 
+  /**
+   * Filters
+   */
+
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addFilter("kebabCase", function(txt) {
+    return kebabCase(txt)
+  });
+
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
+
+  /**
+   * Plugins
+   */
+
+  eleventyConfig.addPlugin(pluginRss);
+
+  /**
+   * Miscellaneous
+   */
+  eleventyConfig.addCollection("tagList", function(collection) {
+    let tagSet = new Set();
+    collection.getAll().forEach(function(item) {
+      if( "tags" in item.data ) {
+        let tags = item.data.tags;
+
+        tags = tags.filter(function(item) {
+          switch(item) {
+            // this list should match the `filter` list in tags.njk
+            case "all":
+            case "nav":
+            case "post":
+            case "posts":
+              return false;
+          }
+
+          return true;
+        });
+
+        for (const tag of tags) {
+          tagSet.add(tag);
+        }
+      }
+    });
+
+    // returning an array in addCollection works in Eleventy 0.5.3
+    return [...tagSet];
   });
 
   eleventyConfig.setBrowserSyncConfig({
